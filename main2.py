e458e47cd26e71d7a249376815c859238365c047
Владимир <50101573+Ihtynet@users.noreply.github.com>
2023-03-05 00:55:48 +0300

    123

---
+++ main2.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template,request
+app = Flask(__name__)
+
+@app.route('/')
+def startpage():
+    return render_template("start.html")
+
+@app.route('/catalog/')
+def catalog():
+   return render_template("catalog.html")
+
+@app.route('/help/')
+def help():
+      return render_template("help.html")
+
+if __name__ == '__main__':
+    app.run()
+
+#host='0.0.0.0'
